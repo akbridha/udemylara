b490381dccebb5d6e562898bea063019ce8e6e1c
PaluiBanar <66150585+akbridha@users.noreply.github.com>
2023-10-04 12:53:46 +0800

    Delete resources directory

--- resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-
-<footer class='bg-dark'>
-    <div class="container">
-            <div class="text-center p-3">
-                <p style="color : white">&copy; 2023 Tampilan Sederhana</p>
-            </div>
-    </div>
-</footer>
